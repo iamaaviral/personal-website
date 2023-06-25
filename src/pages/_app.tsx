1656770cd73c356f02df3196dd736adc4c57f4af
Aviral Srivastava <srivastavaaviral1@gmail.com>
2023-06-25 23:56:15 +0530

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps, NextWebVitalsMetric } from 'next/app'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import "@/styles/globals.css";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import Script from "next/script";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+  `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
